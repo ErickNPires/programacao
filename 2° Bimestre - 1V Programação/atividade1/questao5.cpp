72e3ca65e1c270f4541668bbc82ceb50db25d21d
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 16:59:15 -0300

    Update questao5.cpp

--- 2° Bimestre - 1V Programação/atividade1/questao5.cpp
+++ 2° Bimestre - 1V Programação/atividade1/questao5.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+     float C;
+     float F;
+
+     cout << "Informe a temperatura em Celsius: ";
+     cin >> C;
+
+     F = (9 * C + 160) / 5;
+     
+     cout << "Temperatura: " << F << "°F" << endl; 
+     return 0;
+}
