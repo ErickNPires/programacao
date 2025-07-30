170c6f1eec241e0c2766140b0d9971b8a99dfdbf
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 13:08:51 -0300

    Create questao3.cpp

---
+++ 2° Bimestre - 1V Programação/atividade1/questao3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     int base;
+     int altura;
+     int area;
+
+     cout << "Informe a base do retangulo: ";
+     cin >> base;
+
+     cout << "Informe a altura do retangulo: ";
+     cin >> altura;
+
+     area = base * altura;
+     
+     cout << "Area do retangulo: " << area << endl; 
+     return 0;
+}
