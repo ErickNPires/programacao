9ab3166e2c7eef2988eeb7e49326b95554fbb1a1
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 13:03:35 -0300

    Update questao1.cpp

--- 2° Bimestre - 1V Programação/atividade1/questao1.cpp
+++ 2° Bimestre - 1V Programação/atividade1/questao1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+     int num1;
+     int num2;
+     int soma;
+
+     cout << "Informe o primeiro número: ";
+     cin >> num1;
+
+     cout << "Informe o segundo número: ";
+     cin >> num2;
+
+     soma = num + num2;
+     
+     cout << "Soma dos dois numeros: " << soma << endl; 
+     return 0;
+}
