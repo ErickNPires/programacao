09cda98da2e014ae26fb89fd251781c049cbc35e
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 17:12:13 -0300

    Create questao8.cpp

---
+++ 2° Bimestre - 1V Programação/atividade1/questao8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     float preco1;
+     float preco2;
+     float preco3;
+     float total;
+
+     cout << "Informe o primeiro preco: ";
+     cin >> preco1;
+
+     cout << "Informe o segundo preco: ";
+     cin >> preco2;
+
+     cout << "Informe o terceiro preco: ";
+     cin >> preco3;
+
+     total = preco1 + preco2 + preco3;
+
+     cout << "Preco total : " << total << endl; 
+     return 0;
+}
