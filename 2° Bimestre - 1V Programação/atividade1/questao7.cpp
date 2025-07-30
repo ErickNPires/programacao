39b5edb6220c4eb8f9c2e149e7f6d3e8a3d21364
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 17:09:35 -0300

    Create questao7.cpp

---
+++ 2° Bimestre - 1V Programação/atividade1/questao7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     float comprimento;
+     float largura;
+     float altura;
+     float volume;
+
+     cout << "Informe o comprimento da caixa: ";
+     cin >> comprimento;
+
+     cout << "Informe a largura da caixa: ";
+     cin >> largura;
+
+     cout << "Informe a altura da caixa: ";
+     cin >> altura;
+  
+     volume = comprimento * largura * altura;
+     
+     cout << "Volume da caixa : " << volume << endl; 
+     return 0;
+}
