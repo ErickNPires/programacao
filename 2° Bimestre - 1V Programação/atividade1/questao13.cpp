ef93ed6fa1fca5bb46edafa6bc68d6bd944d49ae
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 22:44:24 -0300

    Create questao13.cpp

---
+++ 2° Bimestre - 1V Programação/atividade1/questao13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     float C;
+     float i;
+     float t;
+     float M;
+
+     cin >> C;
+
+     cin >> i;
+
+     cin >> t;
+
+     M = C + (C * i * t);
+     
+     return 0;
+}
