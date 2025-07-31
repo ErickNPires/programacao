220957cc6215d60da294675e014b616d1ee12488
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 22:58:02 -0300

    Update questao15.cpp

--- 2° Bimestre - 1V Programação/atividade1/questao15.cpp
+++ 2° Bimestre - 1V Programação/atividade1/questao15.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main () {
-     float peso;
-     float altura;
-     float imc;
+    float peso;
+    float altura;
+    float imc;
 
-     cout << "Informe o peso em kg: ";
-     cin >> peso;
+    cout << "Informe o peso em kg: ";
+    cin >> peso;
 
-     cout << "Informe a altura em m: ";
-     cin >> altura;
+    cout << "Informe a altura em m: ";
+    cin >> altura;
 
-     imc = peso / (altura ** 2);
-     
-     cout << "Valor do IMC: " << imc << endl; 
-     return 0;
+    imc = peso / pow(altura, 2);
+
+    cout << "Valor do IMC: " << imc << endl; 
+    return 0;
 }
