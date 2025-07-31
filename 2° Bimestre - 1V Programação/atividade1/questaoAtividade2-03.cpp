c87318b715572141d0ec1227221323742e9e9165
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-31 16:46:45 -0300

    Create questaoAtividade2-03.cpp

---
+++ 2° Bimestre - 1V Programação/atividade1/questaoAtividade2-03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+    float base;
+    float altura;
+
+    cout << "Informe a base: ";
+    cin >> raio;
+
+    petrimetro = 2 * 3.14 * raio;
+
+    cout << "Valor do IMC: " << imc << endl; 
+    return 0;
+}
