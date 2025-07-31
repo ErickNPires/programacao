62c8d97e7051672b394000585c1534c1f8fcca42
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 22:28:10 -0300

    Create questao10.cpp

---
+++ 2° Bimestre - 1V Programação/atividade1/questao10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+     float dia;
+     float horas;
+
+     cout << "Informe o numero de dias: ";
+     cin >> dia;
+
+     horas = dia * 24;
+
+     cout << "Quantidade de horas: " << total << endl; 
+     return 0;
+}
