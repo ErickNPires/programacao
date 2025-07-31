093fbf152210d24055779c698c2e368c8d9c00ed
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-30 22:25:52 -0300

    Update questao9.cpp

--- 2° Bimestre - 1V Programação/atividade1/questao9.cpp
+++ 2° Bimestre - 1V Programação/atividade1/questao9.cpp
@@ -1,1 +1,23 @@
-// Fazer depois
+#include <iostream>
+using namespace std;
+
+int main() {
+    double tempoHoras;
+    double velocidadeMedia;
+    double distancia;
+    double tempoMinutos;
+
+    cout << "Digite o tempo gasto na viagem (em horas): ";
+    cin >> tempoHoras;
+
+    cout << "Digite a velocidade media do veiculo (em km/h): ";
+    cin >> velocidadeMedia;
+
+    distancia = velocidadeMedia * tempoHoras;
+    tempoMinutos = tempoHoras * 60;
+
+    cout << "\nDistancia total percorrida: " << distancia << " km" << endl;
+    cout << "Tempo total em minutos: " << tempoMinutos << " minutos" << endl;
+
+    return 0;
+}
