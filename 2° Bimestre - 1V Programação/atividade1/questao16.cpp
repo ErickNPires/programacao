cdfcc8befffe57d6f4701a62f448f894eb06be5e
ErickNPires <erick.pires@escolar.ifrn.edu.br>
2025-07-31 16:30:15 -0300

    Create questao16.cpp

---
+++ 2° Bimestre - 1V Programação/atividade1/questao16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+    float raio;
+    float perimetro;
+
+    cout << "Informe o raio de um circulo: ";
+    cin >> raio;
+
+    petrimetro = 2 * 3.14 * raio;
+
+    cout << "Perimetro: " << perimetro << endl; 
+    return 0;
+}
